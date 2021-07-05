43ab1c846c5183ac9f4e32564218ab14c77c2077
Hanni87 <johannes.klaus@gmx.net>
2021-07-05 16:52:56 +0200

    2 files modified

--- index.js
+++ index.js
@@ -6,22 +6,64 @@ import LeaderLine from 'leader-line-new';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>Processor Debugger</h1>`;
 
-
 new LeaderLine(
   document.getElementById('item1'),
   document.getElementById('item5'),
-  {startSocket: 'bottom', endSocket: 'top', path: 'grid'}
+  { startSocket: 'bottom', endSocket: 'top', path: 'grid' }
 );
 
+new LeaderLine(
+  document.getElementById('item1'),
+  document.getElementById('item4'),
+  { startSocket: 'bottom', endSocket: 'top', path: 'grid' }
+);
+
+new LeaderLine(
+  document.getElementById('item4'),
+  document.getElementById('item7'),
+  { startSocket: 'bottom', endSocket: 'top', path: 'grid' }
+);
+
+new LeaderLine(
+  document.getElementById('item7'),
+  document.getElementById('item10'),
+  { startSocket: 'bottom', endSocket: 'top', path: 'grid' }
+);
 
 new LeaderLine(
   document.getElementById('item2'),
   document.getElementById('item5'),
-  {startSocket: 'bottom', endSocket: 'top', path: 'grid', dash: {animation: true}}
+  {
+    color: 'red',
+    startSocket: 'bottom',
+    endSocket: 'top',
+    path: 'grid',
+    dash: { animation: true }
+  }
 );
 
 new LeaderLine(
   document.getElementById('item5'),
   document.getElementById('item9'),
-  {startSocket: 'bottom', endSocket: 'top', path: 'grid', dash: {animation: true}}
+  {
+    color: 'red',
+    startSocket: 'bottom',
+    endSocket: 'top',
+    path: 'grid',
+    dash: { animation: true }
+  }
 );
+
+let line = new LeaderLine(
+  document.getElementById('item9'),
+  document.getElementById('item10'),
+  {
+    color: 'red',
+    startSocket: 'bottom',
+    endSocket: 'top',
+    path: 'grid',
+    dash: { animation: true }
+  }
+);
+
+// line.remove()
